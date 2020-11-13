f656d554e6928d36e4fb159e601640762ec6d51e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-11-13 12:59:57 +0200

    Initial Commit

---
+++ CompassSwiftUI/CompassSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompassSwiftUIApp.swift
+//  CompassSwiftUI
+//
+//  Created by ramil on 13.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct CompassSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
